7475e50a923036255cb524b53a5e529f4618a565
biswapanda <biswa.panda@gmail.com>
2019-09-09 15:30:32 -0700

    --wip-- [skip ci]

---
+++ cmake-build-debug/CMakeFiles/ae.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/bpanda/dev/redis/src/ae.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/ae.dir/src/ae.c.o"
+  "/Users/bpanda/dev/redis/src/anet.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/ae.dir/src/anet.c.o"
+  "/Users/bpanda/dev/redis/src/zmalloc.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/ae.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/my-redis-server.dir/DependInfo.cmake
@@ -0,0 +1,87 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/bpanda/dev/redis/src/adlist.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/adlist.c.o"
+  "/Users/bpanda/dev/redis/src/ae.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/ae.c.o"
+  "/Users/bpanda/dev/redis/src/anet.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/anet.c.o"
+  "/Users/bpanda/dev/redis/src/aof.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/aof.c.o"
+  "/Users/bpanda/dev/redis/src/bio.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/bio.c.o"
+  "/Users/bpanda/dev/redis/src/bitops.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/bitops.c.o"
+  "/Users/bpanda/dev/redis/src/blocked.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/blocked.c.o"
+  "/Users/bpanda/dev/redis/src/childinfo.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/childinfo.c.o"
+  "/Users/bpanda/dev/redis/src/cluster.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/cluster.c.o"
+  "/Users/bpanda/dev/redis/src/config.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/config.c.o"
+  "/Users/bpanda/dev/redis/src/crc16.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/crc16.c.o"
+  "/Users/bpanda/dev/redis/src/crc64.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/crc64.c.o"
+  "/Users/bpanda/dev/redis/src/db.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/db.c.o"
+  "/Users/bpanda/dev/redis/src/debug.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/debug.c.o"
+  "/Users/bpanda/dev/redis/src/defrag.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/defrag.c.o"
+  "/Users/bpanda/dev/redis/src/dict.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/dict.c.o"
+  "/Users/bpanda/dev/redis/src/endianconv.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/endianconv.c.o"
+  "/Users/bpanda/dev/redis/src/evict.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/evict.c.o"
+  "/Users/bpanda/dev/redis/src/expire.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/expire.c.o"
+  "/Users/bpanda/dev/redis/src/geo.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/geo.c.o"
+  "/Users/bpanda/dev/redis/src/geohash.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/geohash.c.o"
+  "/Users/bpanda/dev/redis/src/geohash_helper.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/geohash_helper.c.o"
+  "/Users/bpanda/dev/redis/src/hyperloglog.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/hyperloglog.c.o"
+  "/Users/bpanda/dev/redis/src/intset.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/intset.c.o"
+  "/Users/bpanda/dev/redis/src/latency.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/latency.c.o"
+  "/Users/bpanda/dev/redis/src/lazyfree.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/lazyfree.c.o"
+  "/Users/bpanda/dev/redis/src/lzf_c.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/lzf_c.c.o"
+  "/Users/bpanda/dev/redis/src/lzf_d.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/lzf_d.c.o"
+  "/Users/bpanda/dev/redis/src/memtest.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/memtest.c.o"
+  "/Users/bpanda/dev/redis/src/module.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/module.c.o"
+  "/Users/bpanda/dev/redis/src/multi.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/multi.c.o"
+  "/Users/bpanda/dev/redis/src/networking.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/networking.c.o"
+  "/Users/bpanda/dev/redis/src/notify.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/notify.c.o"
+  "/Users/bpanda/dev/redis/src/object.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/object.c.o"
+  "/Users/bpanda/dev/redis/src/pqsort.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/pqsort.c.o"
+  "/Users/bpanda/dev/redis/src/pubsub.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/pubsub.c.o"
+  "/Users/bpanda/dev/redis/src/quicklist.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/quicklist.c.o"
+  "/Users/bpanda/dev/redis/src/rand.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/rand.c.o"
+  "/Users/bpanda/dev/redis/src/rax.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/rax.c.o"
+  "/Users/bpanda/dev/redis/src/rdb.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/rdb.c.o"
+  "/Users/bpanda/dev/redis/src/redis-check-aof.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/redis-check-aof.c.o"
+  "/Users/bpanda/dev/redis/src/redis-check-rdb.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/redis-check-rdb.c.o"
+  "/Users/bpanda/dev/redis/src/release.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/release.c.o"
+  "/Users/bpanda/dev/redis/src/replication.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/replication.c.o"
+  "/Users/bpanda/dev/redis/src/rio.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/rio.c.o"
+  "/Users/bpanda/dev/redis/src/scripting.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/scripting.c.o"
+  "/Users/bpanda/dev/redis/src/sds.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/sds.c.o"
+  "/Users/bpanda/dev/redis/src/sentinel.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/sentinel.c.o"
+  "/Users/bpanda/dev/redis/src/server.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/server.c.o"
+  "/Users/bpanda/dev/redis/src/setproctitle.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/setproctitle.c.o"
+  "/Users/bpanda/dev/redis/src/sha1.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/sha1.c.o"
+  "/Users/bpanda/dev/redis/src/siphash.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/siphash.c.o"
+  "/Users/bpanda/dev/redis/src/slowlog.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/slowlog.c.o"
+  "/Users/bpanda/dev/redis/src/sort.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/sort.c.o"
+  "/Users/bpanda/dev/redis/src/sparkline.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/sparkline.c.o"
+  "/Users/bpanda/dev/redis/src/syncio.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/syncio.c.o"
+  "/Users/bpanda/dev/redis/src/t_hash.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/t_hash.c.o"
+  "/Users/bpanda/dev/redis/src/t_list.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/t_list.c.o"
+  "/Users/bpanda/dev/redis/src/t_set.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/t_set.c.o"
+  "/Users/bpanda/dev/redis/src/t_string.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/t_string.c.o"
+  "/Users/bpanda/dev/redis/src/t_zset.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/t_zset.c.o"
+  "/Users/bpanda/dev/redis/src/util.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/util.c.o"
+  "/Users/bpanda/dev/redis/src/ziplist.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/ziplist.c.o"
+  "/Users/bpanda/dev/redis/src/zipmap.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/zipmap.c.o"
+  "/Users/bpanda/dev/redis/src/zmalloc.c" "/Users/bpanda/dev/redis/cmake-build-debug/CMakeFiles/my-redis-server.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/hiredis"
+  "../deps/linenoise"
+  "../deps/lua/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
